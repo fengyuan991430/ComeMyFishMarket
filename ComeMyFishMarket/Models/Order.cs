a3e1821a7d010702ba9e823fa838299902bcc536
TING FENG YUAN <TP046268@mail.apu.edu.my>
2021-07-12 16:05:38 +0800

    Reset

--- ComeMyFishMarket/Models/Order.cs
+++ ComeMyFishMarket/Models/Order.cs
@@ -13,9 +13,5 @@ namespace ComeMyFishMarket.Models
         public string OrderStatus { get; set; }
         public DateTime OrderDate { get; set; }
         public string UserID { get; set; }
-        
-        //command
-        //yohhoooo
-        
     }
 }
