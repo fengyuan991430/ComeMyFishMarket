9192418bedf7cbee5a15cbeb5bf4600a05a76a19
TING FENG YUAN <TP046268@mail.apu.edu.my>
2021-07-22 13:08:20 +0800

    te

--- ComeMyFishMarket/Migrations/ComeMyFishMarketClass/20210719043705_updatedbs.Designer.cs
+++ ComeMyFishMarket/Migrations/ComeMyFishMarketClass/20210719043705_updatedbs.Designer.cs
@@ -4,14 +4,16 @@ using ComeMyFishMarket.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace ComeMyFishMarket.Migrations.ComeMyFishMarketClass
 {
     [DbContext(typeof(ComeMyFishMarketClassContext))]
-    partial class ComeMyFishMarketClassContextModelSnapshot : ModelSnapshot
+    [Migration("20210719043705_updatedbs")]
+    partial class updatedbs
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
